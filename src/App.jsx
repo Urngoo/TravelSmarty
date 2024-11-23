c18fb320a46b53b1f14f4e6b7efe6f7306bcccd3
Urngoo <urngoariunbaatar@gmail.com>
2024-11-23 16:23:06 +0800

    pages component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-import SideBar from "./dashboard/Sidebar";
+import SideBar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);//test hiiv
+  const [count, setCount] = useState(0); //test hiiv
 
   return (
     <>
-      <SideBar/>
+      <SideBar />
+      <Navbar />
     </>
   );
 }
