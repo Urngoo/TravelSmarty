b066c69281294d094f6dbd6def89636f7d752935
Myagmarsuren014 <myagmarsuren014@gmail.com>
2024-12-05 08:08:30 +0800

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,22 @@ import Dashboard from "./Pages/Dashboard";
 import Inbox from "./Pages/Inbox";
 import Integration from "./Pages/Integration";
 import Map from "./Pages/Map";
-import AddEvent from "./component/event";
-import SideBar from "./dashboard/Sidebar";
 import Flight from "./component/flight";
-
-
 
 function App() {
   const [count, setCount] = useState(0); //test hiiv
 
   return (
-    <>
-      <CardComponent />
-      <ToggleSwitch />
-      <SideBar/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/integration" element={<Integration />} />
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/flight" element={<Flight />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
