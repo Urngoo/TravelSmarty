75c34c324f09bacfd7c92e2e75417b51cdb3f21e
Myagmarsuren014 <myagmarsuren014@gmail.com>
2024-12-06 08:16:26 +0800

    dashboard lttle change

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
 import WeatherCard from "../dashboard/WeatherCard";
 import ToggleSwitch from "../dashboard/ToggleSwitch";
 import PageLayout from "../PageLayout";
-import Navbar from "../components/Navbar";
 
 function Dashboard() {
-  const [count, setCount] = useState(0); //test hiiv
-
   return (
     <>
       <PageLayout>
-        {/* <Navbar /> */}
         <ToggleSwitch />
         <WeatherCard />
       </PageLayout>
