2d14afa42ec5df1e3ade9efa373e0c5a6df41732
Urngoo <urngoariunbaatar@gmail.com>
2024-12-25 22:53:42 +0800

    ignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,31 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {
-  ClerkProvider,
-  RedirectToSignIn,
-  SignedIn,
-  SignedOut,
-  UserButton,
-  SignInButton,
-  useUser,
-} from "@clerk/clerk-react"; // Import Clerk components
 import "./App.css";
 import Calendar from "./Pages/Cal";
 import Dashboard from "./Pages/Dashboard";
 import Inbox from "./Pages/Inbox";
 import Integration from "./Pages/Integration";
-import Map from "./pages/Map";
+import Map from "./Pages/Map";
 import Flight from "./components/flight";
 import SideBar from "./components/Sidebar";
 import WeatherCard from "./dashboard/WeatherCard";
 
-function ProtectedRoute({ children }) {
-  const { isLoaded, isSignedIn } = useUser();
-
-  if (!isLoaded) {
-    return <div>Loading...</div>;
-  }
-
-  if (!isSignedIn) {
-    return <RedirectToSignIn />;
-  }
-
-  return children;
-}
-
 function App() {
-  const { user } = useUser();
-  const [collapsed, setCollapsed] = useState(false);
-
   return (
     <BrowserRouter>
       <div className="flex">
-        <SideBar collapsed={collapsed} setCollapsed={setCollapsed} />
-
+        <SideBar />
         <div className={`flex flex-col transition-all duration-300 w-full`}>
           {/* Main content */}
           <div className="flex-grow p-4">
             <Routes>
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/calendar"
-                element={
-                  <ProtectedRoute>
-                    <Calendar />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/integration"
-                element={
-                  <ProtectedRoute>
-                    <Integration />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/inbox"
-                element={
-                  <ProtectedRoute>
-                    <Inbox />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/map"
-                element={
-                  <ProtectedRoute>
-                    <Map />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/flight"
-                element={
-                  <ProtectedRoute>
-                    <Flight />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/WeatherCard"
-                element={
-                  <ProtectedRoute>
-                    <WeatherCard />
-                  </ProtectedRoute>
-                }
-              />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/integration" element={<Integration />} />
+              <Route path="/inbox" element={<Inbox />} />
+              <Route path="/map" element={<Map />} />
+              <Route path="/flight" element={<Flight />} />
+              <Route path="/WeatherCard" element={<WeatherCard />} />
             </Routes>
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
 import App from "./App.jsx";
 
-const clerk_key = import.meta.env.VITE_CLERK_KEY;
-
-if (!clerk_key) {
-  throw new Error("Clerk publishable key is missing");
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={clerk_key}>
-      <App />
-    </ClerkProvider>
+    <App />
   </StrictMode>
 );
